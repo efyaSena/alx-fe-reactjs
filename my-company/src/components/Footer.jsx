72a667fb77564398cbc229b1e0bd1642b8d7840c
efya sena <kumedzrosenagifty@gmail.com>
2025-11-09 23:51:33 +0000

    add all files

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© {new Date().getFullYear()} My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
