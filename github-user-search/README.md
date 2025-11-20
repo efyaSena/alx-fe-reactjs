13fcf55f672fbca55b3229aa044f5972a3db3e5f
efya sena <kumedzrosenagifty@gmail.com>
2025-11-20 01:36:39 +0000

    add all files

---
+++ github-user-search/README.md
@@ -0,0 +1,11 @@
+# GitHub User Search Application
+
+This project is a React application that allows users to search for GitHub profiles using the GitHub API.  
+
+## Project Setup
+
+- Created with Vite + React
+- Axios installed for API calls
+- `components/` folder for React components
+- `services/` folder for API service functions
+- Environment variables stored in `.env`
---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,28 @@
+// import axios from "axios";
+
+// // const BASE_URL = "https://api.github.com/users/";
+
+// export const fetchGitHubUser = async (username) => {
+//   if (!username) return null;
+//   try {
+//     const response = await axios.get(`${BASE_URL}${username}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching GitHub user:", error);
+//     return null;
+//   }
+// // };
+// // 
+
+
+
+import axios from "axios";
+import React from 'react'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
