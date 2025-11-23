afb47ea25404512dd410b38c06223bad35f05dae
Efya-Sena <kumedzrosenagifty@gmail.com>
2025-11-23 21:02:16 +0000

    add all files

---
+++ github-user-search/postcss.config.cjs
@@ -0,0 +1,14 @@
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+
+// import autoprefixer from "autoprefixer"
+
+// };
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer : {},
+  },
+};
