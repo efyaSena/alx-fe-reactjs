d8171f17b8663aca6ac7cf61b13b1e0eb53c909b
efyaSena <kumedzrosenagifty@gmail.com>
2025-12-15 21:09:37 +0000

    add all files

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,33 @@
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Profile from "./components/Profile.jsx";
+import ProfileDetails from "./pages/ProfileDetails.jsx";
+import ProfileSettings from "./pages/ProfileSettings.jsx";
+import BlogPost from "./pages/BlogPost.jsx";
+import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
 
-<Route element={<ProtectedRoute />}>
-  <Route path="/profile" element={<Profile />} />
-</Route>
+        
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+      
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+       
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
